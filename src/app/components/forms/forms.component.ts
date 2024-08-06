c970ab5c5ea165451b3d8e3e87bbab32e087bac9
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-08-06 16:34:41 -0300

    feat: create form structure and style

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent implements OnInit{
+
+  results = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
