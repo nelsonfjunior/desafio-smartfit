bb51ed10fe17c8d0ccc1b44fb495365c8baddb98
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-08-08 19:33:26 -0300

    feat: create services for connect with Api

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -2,6 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
+import { FilterUnitsService } from '../../services/filter-units.service';
+
+const OPENING_HOURS = {
+  morning: {
+    first: '06',
+    last: '12'
+  },
+  afternoon: {
+    first: '12',
+    last: '18'
+  },
+  night: {
+    first: '18',
+    last: '23'
+  }
+}
+
+type HOUR_INDEXES = 'morning' | 'afternoon' | 'night';
 
 @Component({
   selector: 'app-forms',
@@ -14,7 +32,10 @@ export class FormsComponent implements OnInit{
   filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private unitService: GetUnitsService,
+    private filterUnitsService: FilterUnitsService) { }
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
@@ -29,12 +50,8 @@ export class FormsComponent implements OnInit{
   }
 
   onSubmit(): void {
-    if(!this.formGroup.value.showClosed){
-      this.filteredResults = this.results.filter(location => location.opened == true);
-    }else{
-      this.filteredResults = this.results;
-    }
-
+    let {showClosed, hour} = this.formGroup.value;
+    this.filteredResults = this.filterUnitsService.filter(this.results, hour, showClosed);
   }
 
   onClean(): void {
